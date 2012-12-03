794cc59bfa6ede28285e1712b47de771a2c0a95c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-03 11:50:51 +0100

    Initialisation of the database

---
+++ iteach-service-impl/src/main/java/net/iteach/service/db/SQLUtils.java
@@ -0,0 +1,41 @@
+package net.iteach.service.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public final class SQLUtils {
+
+	private SQLUtils() {
+	}
+
+	public static Timestamp now() {
+		return toTimestamp(DateTime.now(DateTimeZone.UTC));
+	}
+
+	public static Timestamp toTimestamp(DateTime dateTime) {
+		return new Timestamp(dateTime.getMillis());
+	}
+
+	public static DateTime getDateTime(ResultSet rs, String columnName) throws SQLException {
+		Timestamp timestamp = rs.getTimestamp(columnName);
+		return getDateTime(timestamp);
+	}
+
+	public static DateTime getDateTime(Timestamp timestamp) {
+		return new DateTime(timestamp.getTime(), DateTimeZone.UTC);
+	}
+
+	public static <E extends Enum<E>> E getEnum(Class<E> enumClass, ResultSet rs, String columnName) throws SQLException {
+		String value = rs.getString(columnName);
+		if (value == null) {
+			return null;
+		} else {
+			return Enum.valueOf(enumClass, value);
+		}
+	}
+
+}
