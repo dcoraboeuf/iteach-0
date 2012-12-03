3401a103ce51b15693ef49078812f8c54693d6d8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-03 18:16:21 +0100

    Integration tests

---
+++ iteach-service-impl/src/test/groovy/net/iteach/service/impl/SchoolServiceImplTest.groovy
@@ -0,0 +1,22 @@
+package net.iteach.service.impl
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.iteach.api.SchoolService;
+import net.iteach.test.AbstractIntegrationTest;
+
+class SchoolServiceImplTest extends AbstractIntegrationTest {
+	
+	@Autowired
+	private SchoolService service
+	
+	@Test
+	void getSchools() {
+		def schools = service.getSchoolsForTeacher(1)
+		assert schools != null
+		assert [1, 3] == schools.getSummaries()*.getId()
+		assert ["My school 1", "My school 3"] == schools.getSummaries()*.getName()
+	}
+
+}
