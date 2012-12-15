da96f0052c4441adf9872b6a17f6f70551476b59
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-15 21:22:02 +0100

    Export of the strings

--- iteach-web/src/main/java/net/iteach/web/config/WebConfig.java
+++ iteach-web/src/main/java/net/iteach/web/config/WebConfig.java
@@ -1,6 +1,7 @@
 package net.iteach.web.config;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Locale;
 
 import net.sf.jstring.Strings;
@@ -8,6 +9,7 @@ import net.sf.jstring.support.StringsLoader;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
 
 @Configuration
 public class WebConfig {
@@ -15,6 +17,13 @@ public class WebConfig {
 	@Bean
 	public Strings strings() throws IOException {
 		return StringsLoader.auto(Locale.ENGLISH, Locale.FRENCH, Locale.GERMAN);
+	}
+	
+	@Bean
+	public Object exporter() throws IOException {
+		MBeanExporter exporter = new MBeanExporter();
+		exporter.setBeans(Collections.<String,Object>singletonMap("bean:name=strings", strings()));
+		return exporter;
 	}
 	
 }
