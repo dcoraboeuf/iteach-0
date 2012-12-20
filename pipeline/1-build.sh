8f89daaf5b1eb8efd456d4c386894f5a31671253
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-20 18:06:29 +0100

    #16 Deployment script

---
+++ pipeline/1-build.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Pre-requisites:
+
+#############################
+# Check environment variables
+#############################
+
+if [ "$BUILD_NUMBER" == "" ]
+then
+	echo BUILD_NUMBER is required.
+	exit 1
+fi
+
+if [ "$MVN" == "" ]
+then
+	echo MVN is required.
+	exit 1
+fi
+
+if [ "$NEXUS_URL" == "" ]
+then
+        echo NEXUS_URL is required.
+        exit 1
+fi
+
+if [ "$NEXUS_ID" == "" ]
+then
+        echo NEXUS_ID is required.
+        exit 1
+fi
+
+##########################
+# General MVN options
+##########################
+
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128m -Djava.net.preferIPv4Stack=true"
+
+##########################
+# Preparation of the build
+##########################
+
+# Gets the version number from the POM
+VERSION=`mvn help:evaluate -Dexpression=project.version $MVN_OPTIONS | grep -E "^[A-Za-z\.0-9]+-SNAPSHOT$" | sed -e 's/\-SNAPSHOT//'`
+
+# Release number is made of the version and the build number
+RELEASE=${VERSION}-${BUILD_NUMBER}
+echo Building release ${RELEASE}...
+
+
+#####################################################
+# Runs the build itself and as much tests as possible
+#####################################################
+
+# Clean
+git checkout -- .
+
+# Changing the versions
+mvn versions:set -DnewVersion=${RELEASE} -DgenerateBackupPoms=false
+
+# Maven build
+mvn clean install
+if [ $? -ne 0 ]
+then
+	echo Build failed.
+	exit 1
+fi
+
+# Creates the TAR that contains:
+# 1. the WAR
+# 2. all the deployment files
+echo Creating the archive...
+tar --create --file=target/iteach-${RELEASE}.tar --directory=iteach-web/target iteach-web-${RELEASE}.war
+tar --update --file=target/iteach-${RELEASE}.tar pipeline/*.*
+if [ $? -ne 0 ]
+then
+	echo Archiving failed.
+	exit 1
+fi
+
+###############################
+# Upload the archive into Nexus
+###############################
+
+echo Uploading to Nexus @ ${NEXUS_URL} with id = ${NEXUS_ID}
+mvn deploy:deploy-file -Dfile=target/iteach-${RELEASE}.tar -DrepositoryId=${NEXUS_ID} -Durl=${NEXUS_URL} -DgroupId=net.iteach -DartifactId=iteach -Dversion=${RELEASE} -DgeneratePom=true -Dpackaging=tar
+if [ $? -ne 0 ]
+then
+	echo Deployment failed.
+	exit 1
+fi
+
+
+##################################################################
+# After the build is complete, create the tag in Git and pushes it
+##################################################################
+
+echo Tagging...
+
+# We don't care about Jenkins tags
+for TAG in $(git tag -l jenkins-myconfig*) ; do
+	git tag -d $TAG
+done
+
+# Tag
+TAG=iteach-${RELEASE}
+# Tagging the build
+git tag ${TAG}
+# Pushing the result
+git push origin ${TAG}
+
+echo Done.
