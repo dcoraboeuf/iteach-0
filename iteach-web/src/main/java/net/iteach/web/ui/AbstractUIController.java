e87eb1ea5f2a1193facdc3805f208172aea73836
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-21 15:10:40 +0100

    #23 Creation of an abstract level for the UI controller

---
+++ iteach-web/src/main/java/net/iteach/web/ui/AbstractUIController.java
@@ -0,0 +1,70 @@
+package net.iteach.web.ui;
+
+import net.iteach.core.security.SecurityUtils;
+import net.iteach.core.ui.TeacherUI;
+import net.iteach.utils.InputException;
+import net.iteach.web.support.ErrorHandler;
+import net.iteach.web.support.ErrorMessage;
+import net.sf.jstring.Strings;
+import net.sf.jstring.support.CoreException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+public abstract class AbstractUIController implements TeacherUI {
+
+	protected final SecurityUtils securityUtils;
+	private final ErrorHandler errorHandler;
+	private final Strings strings;
+
+	@Autowired
+	public AbstractUIController(SecurityUtils securityUtils,
+                                ErrorHandler errorHandler,
+                                Strings strings) {
+		this.securityUtils = securityUtils;
+		this.errorHandler = errorHandler;
+		this.strings = strings;
+	}
+
+	protected ResponseEntity<String> getMessageResponse(String message) {
+		// Header
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+		// OK
+		return new ResponseEntity<String>(message, responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(InputException.class)
+	public ResponseEntity<String> onInputException (HttpServletRequest request, Locale locale, InputException ex) {
+		// Returns a message to display to the user
+		String message = ex.getLocalizedMessage(strings, locale);
+		// OK
+		return getMessageResponse(message);
+	}
+
+	@ExceptionHandler(CoreException.class)
+	public ResponseEntity<String> onCoreException (HttpServletRequest request, Locale locale, CoreException ex) {
+		// Error message
+		ErrorMessage error = errorHandler.handleError (request, locale, ex);
+		// Returns a message to display to the user
+		String message = strings.get(locale, "general.error", error.getMessage(), error.getUuid());
+		// Ok
+		return getMessageResponse(message);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> onException (HttpServletRequest request, Locale locale, Exception ex) {
+		// Error message
+		ErrorMessage error = errorHandler.handleError (request, locale, ex);
+		// Returns a message to display to the user
+		String message = strings.get(locale, "general.failure", error.getUuid());
+		// Ok
+		return getMessageResponse(message);
+	}
+
+}
--- iteach-web/src/main/java/net/iteach/web/ui/TeacherUIController.java
+++ iteach-web/src/main/java/net/iteach/web/ui/TeacherUIController.java
@@ -1,46 +1,21 @@
 package net.iteach.web.ui;
 
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-
 import net.iteach.api.SchoolService;
 import net.iteach.api.StudentService;
-import net.iteach.core.model.Ack;
-import net.iteach.core.model.ID;
-import net.iteach.core.model.SchoolForm;
-import net.iteach.core.model.SchoolSummaries;
-import net.iteach.core.model.StudentForm;
-import net.iteach.core.model.StudentSummaries;
+import net.iteach.core.model.*;
 import net.iteach.core.security.SecurityUtils;
-import net.iteach.core.ui.TeacherUI;
-import net.iteach.utils.InputException;
 import net.iteach.web.support.ErrorHandler;
-import net.iteach.web.support.ErrorMessage;
 import net.sf.jstring.Strings;
-import net.sf.jstring.support.CoreException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/ui/teacher")
-public class TeacherUIController implements TeacherUI {
+public class TeacherUIController extends AbstractUIController {
 
 	private final SchoolService schoolService;
 	private final StudentService studentService;
-	private final SecurityUtils securityUtils;
-	private final ErrorHandler errorHandler;
-	private final Strings strings;
 
 	@Autowired
 	public TeacherUIController(SchoolService schoolService,
@@ -48,47 +23,9 @@ public class TeacherUIController implements TeacherUI {
 			SecurityUtils securityUtils,
 			ErrorHandler errorHandler,
 			Strings strings) {
+        super(securityUtils, errorHandler, strings);
 		this.schoolService = schoolService;
 		this.studentService = studentService;
-		this.securityUtils = securityUtils;
-		this.errorHandler = errorHandler;
-		this.strings = strings;
-	}
-
-	protected ResponseEntity<String> getMessageResponse(String message) {
-		// Header
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
-		// OK
-		return new ResponseEntity<String>(message, responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@ExceptionHandler(InputException.class)
-	public ResponseEntity<String> onInputException (HttpServletRequest request, Locale locale, InputException ex) {
-		// Returns a message to display to the user
-		String message = ex.getLocalizedMessage(strings, locale);
-		// OK
-		return getMessageResponse(message);
-	}
-
-	@ExceptionHandler(CoreException.class)
-	public ResponseEntity<String> onCoreException (HttpServletRequest request, Locale locale, CoreException ex) {
-		// Error message
-		ErrorMessage error = errorHandler.handleError (request, locale, ex);
-		// Returns a message to display to the user
-		String message = strings.get(locale, "general.error", error.getMessage(), error.getUuid());
-		// Ok
-		return getMessageResponse(message);
-	}
-
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> onException (HttpServletRequest request, Locale locale, Exception ex) {
-		// Error message
-		ErrorMessage error = errorHandler.handleError (request, locale, ex);
-		// Returns a message to display to the user
-		String message = strings.get(locale, "general.failure", error.getUuid());
-		// Ok
-		return getMessageResponse(message);
 	}
 
 	@Override
