9ffbdbd5ea7af97200bafa0892abf811d8c83bf6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-21 12:55:21 +0100

    #23 Validation of StudentForm

--- iteach-service-api/src/main/java/net/iteach/api/StudentService.java
+++ iteach-service-api/src/main/java/net/iteach/api/StudentService.java
@@ -4,15 +4,19 @@ import net.iteach.core.model.Ack;
 import net.iteach.core.model.ID;
 import net.iteach.core.model.StudentForm;
 import net.iteach.core.model.StudentSummaries;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+
+@Validated
 public interface StudentService {
 
 	StudentSummaries getStudentsForTeacher(int teacherId);
 
-	ID createStudentForTeacher(int teacherId, StudentForm form);
+	ID createStudentForTeacher(int teacherId, @Valid StudentForm form);
 
 	Ack deleteStudentForTeacher(int teacherId, int id);
 
-	Ack editStudentForTeacher(int userId, int id, StudentForm form);
+	Ack editStudentForTeacher(int userId, int id, @Valid StudentForm form);
 
 }
