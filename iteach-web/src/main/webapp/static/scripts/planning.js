a3532d302704bbfa54e4dddb35043b5a11700b30
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-27 20:09:03 +0100

    #7 [wd] Initialization of the calendar

---
+++ iteach-web/src/main/webapp/static/scripts/planning.js
@@ -0,0 +1,35 @@
+var Planning = function () {
+
+	function init () {
+		var view = "week";           
+		var DATA_FEED_URL = "php/datafeed.php";
+		var op = {
+			view: view,
+			theme:3,
+			showday: new Date(),
+			//EditCmdhandler:Edit,
+			//DeleteCmdhandler:Delete,
+			//ViewCmdhandler:View,    
+			//onWeekOrMonthToDay:wtd,
+			//onBeforeRequestData: cal_beforerequest,
+			//onAfterRequestData: cal_afterrequest,
+			//onRequestDataError: cal_onerror, 
+			autoload:true,
+			//url: DATA_FEED_URL + "?method=list",  
+			//quickAddUrl: DATA_FEED_URL + "?method=add", 
+			//quickUpdateUrl: DATA_FEED_URL + "?method=update",
+			//quickDeleteUrl: DATA_FEED_URL + "?method=remove"        
+		};
+		
+		op.height = $('#home-row').height() - $('planning-panel-title').height() - 50;
+		
+		var p = $("#planning-calendar").bcalendar(op).BcalGetOp();
+	}
+
+	return {
+		init: init
+	};
+
+} ();
+
+$(document).ready(Planning.init);
