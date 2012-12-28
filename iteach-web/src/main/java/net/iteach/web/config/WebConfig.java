75448b8dfad40566fc1e9fdc42d2f5dbf25898e8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-28 16:38:32 +0100

    #7 Registering JSON configuration for JODA time

--- iteach-web/src/main/java/net/iteach/web/config/WebConfig.java
+++ iteach-web/src/main/java/net/iteach/web/config/WebConfig.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Locale;
 
+import net.iteach.web.support.json.LocalDateDeserializer;
+import net.iteach.web.support.json.LocalDateSerializer;
+import net.iteach.web.support.json.LocalTimeDeserializer;
+import net.iteach.web.support.json.LocalTimeSerializer;
 import net.sf.jstring.Strings;
 import net.sf.jstring.support.StringsLoader;
 
+import org.codehaus.jackson.Version;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.module.SimpleModule;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
@@ -31,7 +39,28 @@ public class WebConfig {
 	
 	@Bean
 	public ObjectMapper jacksonObjectMapper() {
-		return ObjectMapperFactory.createObjectMapper();
+		ObjectMapper mapper = ObjectMapperFactory.createObjectMapper();
+		
+		jsonJoda(mapper);
+		
+		return mapper;
+	}
+
+	protected void jsonJoda(ObjectMapper mapper) {
+		SimpleModule jodaModule = new SimpleModule("JodaTimeModule", new Version(1, 0, 0, null));
+		jsonLocalDate(jodaModule);
+		jsonLocalTime(jodaModule);
+		mapper.registerModule(jodaModule);
+	}
+
+	protected void jsonLocalTime(SimpleModule jodaModule) {
+		jodaModule.addSerializer(LocalTime.class, new LocalTimeSerializer());
+		jodaModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer());
+	}
+
+	protected void jsonLocalDate(SimpleModule jodaModule) {
+		jodaModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+		jodaModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
 	}
 	
 }
