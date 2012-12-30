621e5c5a98ef8516f43b62c4a618eb92e5d95cd4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-12-30 15:17:49 +0100

    #36 Student page skeleton and reorganization of breadcrumbs

---
+++ iteach-web/src/main/java/net/iteach/web/student/StudentController.java
@@ -0,0 +1,31 @@
+package net.iteach.web.student;
+
+import javax.servlet.http.HttpSession;
+
+import net.iteach.core.ui.TeacherUI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gui/student")
+public class StudentController {
+
+	private final TeacherUI ui;
+	
+	@Autowired
+	public StudentController(TeacherUI ui) {
+		this.ui = ui;
+	}
+
+	@RequestMapping("/{id:\\d+}")
+	public String details (@PathVariable int id, Model model, HttpSession session) {
+		// FIXME Loads the student details
+		// View
+		return "student";
+	}
+
+}
