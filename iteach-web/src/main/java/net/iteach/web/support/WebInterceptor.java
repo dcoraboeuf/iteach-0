cb425502b675f0bd13d19b0fa2d9c520a05e6b0a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-08 22:13:16 +0100

    #2 Web UI service

--- iteach-web/src/main/java/net/iteach/web/support/WebInterceptor.java
+++ iteach-web/src/main/java/net/iteach/web/support/WebInterceptor.java
@@ -9,6 +9,24 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component("webInterceptor")
 public class WebInterceptor extends HandlerInterceptorAdapter {
+
+	private static final ThreadLocal<HttpServletRequest> CURRENT_REQUEST = new ThreadLocal<HttpServletRequest>();
+
+	public static HttpServletRequest getCurrentRequest() {
+		HttpServletRequest request = CURRENT_REQUEST.get();
+		if (request == null) {
+			throw new IllegalStateException("No current request");
+		} else {
+			return request;
+		}
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		CURRENT_REQUEST.set(request);
+		// OK
+		return true;
+	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response,
@@ -20,6 +38,11 @@ public class WebInterceptor extends HandlerInterceptorAdapter {
 		}
 		// OK
 		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		CURRENT_REQUEST.set(null);
 	}
 
 }
---
+++ iteach-web/src/main/java/net/iteach/web/support/WebUIService.java
@@ -0,0 +1,66 @@
+package net.iteach.web.support;
+
+import static java.lang.String.format;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.iteach.api.UIService;
+import net.iteach.core.model.TokenType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class WebUIService implements UIService {
+	
+	private final Logger logger = LoggerFactory.getLogger(UIService.class);
+
+	@Override
+	public String getLink(TokenType type, String... components) {
+		// Gets the request from the content
+		HttpServletRequest request = WebInterceptor.getCurrentRequest();
+		// Logs everything about the request
+		// log(request);
+		// Gets the root URL
+		String root = WebUtils.getBaseURL(request);
+		logger.debug("[ui] Root URL: {}", root);
+		// Building the variable part
+		String query;
+		switch (type) {
+		case REGISTRATION:
+			query = format("gui/user/confirm/%s/%s", (Object[]) components);
+			break;
+		default:
+			throw new IllegalStateException("TokenType is not supported: " + type);
+		}
+		// OK
+		return format("%s/%s", root, query);
+	}
+
+	@SuppressWarnings("unused")
+	private void log(HttpServletRequest request) {
+		logger.debug("[request] Context path: {}", request.getContextPath());
+		logger.debug("[request] Server name: {}", request.getServerName());
+		logger.debug("[request] Server port: {}", request.getServerPort());
+		logger.debug("[request] Request URL: {}", request.getRequestURL());
+		logger.debug("[request] Protocol: {}", request.getProtocol());
+		logger.debug("[request] Scheme: {}", request.getScheme());
+		logger.debug("[request] Servlet path: {}", request.getServletPath());
+		@SuppressWarnings("unchecked")
+		Enumeration<String> names = request.getHeaderNames();
+		while (names.hasMoreElements()) {
+			String name = names.nextElement();
+			@SuppressWarnings("unchecked")
+			Enumeration<String> values = request.getHeaders(name);
+			while (values.hasMoreElements()) {
+				String value = values.nextElement();
+				logger.debug("[request] Header {} = {}", name, value);
+			}
+		}
+	}
+
+}
