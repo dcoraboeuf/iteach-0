2117b7c4bc41e13df7248c7249d8fe8e51c798ee
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-17 14:57:37 +0100

    #24 Dissociates DEV & PROD

--- iteach-web/src/main/java/net/iteach/web/locale/LocalisationController.java
+++ iteach-web/src/main/java/net/iteach/web/locale/LocalisationController.java
@@ -4,6 +4,7 @@ import static org.apache.commons.lang3.StringEscapeUtils.escapeEcmaScript;
 import static org.apache.commons.lang3.StringUtils.replace;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
@@ -11,85 +12,55 @@ import java.util.Map;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-import net.iteach.web.support.AbstractGUIController;
-import net.iteach.web.support.ErrorHandler;
 import net.sf.jstring.Strings;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+public abstract class LocalisationController {
 
-@Controller
-public class LocalisationController extends AbstractGUIController {
+	protected final Strings strings;
 
-	private final Strings strings;
-	private final LocalisationUtils localisationUtils;
-
-	@Autowired
-	public LocalisationController(ErrorHandler errorHandler, Strings strings, LocalisationUtils localisationUtils) {
-		super(errorHandler);
+	public LocalisationController(Strings strings) {
 		this.strings = strings;
-		this.localisationUtils = localisationUtils;
 	}
+	
+	public abstract void localisation(Locale locale, HttpServletResponse response, String version) throws IOException;
 
-	@RequestMapping(value = "/localization", method = RequestMethod.GET)
-	public void localisation(Locale locale, HttpServletResponse response)
-			throws IOException {
-		
-		// Locale
-		if (locale == null) {
-			locale = Locale.ENGLISH;
-		}
-		
+	protected String generateJS(Locale locale) {
 		// Restricts the locale
 		locale = strings.getSupportedLocales().filterForLookup(locale);
 		
-		// TODO Cache management
-		
-		// Last generation time for this locale
-		// long lastGenerationTime = localisationUtils.getLastGenerationTime(locale);
-		// long lastUpdateTime = strings.getLastUpdateTime();
-		// logger.debug("[localization] Locale = {}, Generation = {}, Updated = {}", new Object[] {locale, lastGenerationTime, lastUpdateTime});
-	
-		// if (lastGenerationTime >= lastUpdateTime) {
-		// logger.debug("[localization] No need for regeneration (not modified)");
-		// response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
-		// } else {
-			// logger.debug("[localization] Regeneration needed");
-			// Gets the list of key/values
-			Map<String, String> map = strings.getKeyValues(locale);
-			// Output
-			StringBuilder js = new StringBuilder();
-			js.append("// ").append(new Date()).append("\n");
-			js.append("var l = {\n");
-			int i = 0;
-			for (Map.Entry<String, String> entry : map.entrySet()) {
-				String key = entry.getKey();
-				String value = entry.getValue();
-				if (i > 0) {
-					js.append(",\n");
-				}
-				js.append(String.format("'%s': '%s'", key, escape(value)));
-				i++;
+		// Gets the list of key/values
+		Map<String, String> map = strings.getKeyValues(locale);
+		// Output
+		StringBuilder js = new StringBuilder();
+		js.append("// ").append(new Date()).append("\n");
+		js.append("var l = {\n");
+		int i = 0;
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if (i > 0) {
+				js.append(",\n");
 			}
-			js.append("\n};\n");
-			// Content
-			String content = js.toString();
-			// Returns the response as JS
-			byte[] bytes = content.getBytes("UTF-8");
-			response.setContentType("text/javascript");
-			response.setContentLength(bytes.length);
-			ServletOutputStream outputStream = response.getOutputStream();
-			outputStream.write(bytes);
-			outputStream.flush();
-			// Regenerated
-			localisationUtils.regenerated(locale);
-		// }
+			js.append(String.format("'%s': '%s'", key, escape(value)));
+			i++;
+		}
+		js.append("\n};\n");
+		// Content
+		String content = js.toString();
+		return content;
 	}
 
 	protected String escape(String value) {
 		return escapeEcmaScript(replace(value, "''", "'"));
+	}
+
+	protected void writeJS(String content, HttpServletResponse response) throws UnsupportedEncodingException, IOException {
+		byte[] bytes = content.getBytes("UTF-8");
+		response.setContentType("text/javascript");
+		response.setContentLength(bytes.length);
+		ServletOutputStream outputStream = response.getOutputStream();
+		outputStream.write(bytes);
+		outputStream.flush();
 	}
 
 }
