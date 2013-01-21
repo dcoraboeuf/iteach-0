14f5b926f33bf295eb61d3046ce0f53a8341894d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-21 22:10:49 +0100

    #39 Splitting JS between lessons & planning

---
+++ iteach-web/src/main/webapp/static/scripts/lessons.js
@@ -0,0 +1,92 @@
+var Lessons = function () {
+	
+	function deleteLesson (id) {
+		application.confirmAndCall(
+			loc('lesson.delete.prompt'),
+			function () {
+				$.ajax({
+					type: 'DELETE',
+					url: 'ui/teacher/lesson/{0}'.format(id),
+					contentType: 'application/json',
+					dataType: 'json',
+					success: function (data) {
+						if (data.success) {
+							// Going back to the planning
+							location = "gui/home";
+						} else {
+							application.displayError(loc('lesson.delete.error'));
+						}
+					},
+					error: function (jqXHR, textStatus, errorThrown) {
+					  	application.displayAjaxError (loc('lesson.delete.error'), jqXHR, textStatus, errorThrown);
+					}
+				});
+			}
+		);
+	}
+	
+	function editLesson () {
+		var id = $('#lesson-id').val();
+		var student = $('#lesson-student').val();
+		var date = $('#lesson-date').val();
+		var startTime = $('#lesson-from').val();
+		var endTime = $('#lesson-to').val();
+		var location = $('#lesson-location').val();
+		application.dialog({
+			id: 'lesson-dialog',
+			title: loc('lesson.edit'),
+			width: 500,
+			data: {
+				lessonDate: date,
+				lessonFrom: startTime,
+				lessonTo: endTime,
+				lessonStudent: student,
+				lessonLocation: location
+			},
+			submit: {
+				name: loc('general.update'),
+				action: function () {
+					return submitEditLesson (id);
+				}
+			}
+		});		
+	}
+	
+	function submitEditLesson (id) {
+		$.ajax({
+			type: 'PUT',
+			url: 'ui/teacher/lesson/{0}'.format(id),
+			contentType: 'application/json',
+			data: JSON.stringify({
+				date: $('#lessonDate').val(),
+				from: $('#lessonFrom').val(),
+				to: $('#lessonTo').val(),
+				student: $('#lessonStudent').val(),
+				location: $('#lessonLocation').val()
+			}),
+			dataType: 'json',
+			success: function (data) {
+				if (data.success) {
+					location.reload();
+				} else {
+					application.displayError(loc('lesson.edit.error'));
+				}
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+			  	if (jqXHR.responseText && jqXHR.responseText != '') {
+			  		$('#lesson-dialog-error').html(jqXHR.responseText.htmlWithLines());
+			  		$('#lesson-dialog-error').show();
+			  	} else {
+			  		application.displayAjaxError (loc('lesson.edit.error'), jqXHR, textStatus, errorThrown);
+			  	}
+			}
+		});
+		return false;
+	}
+
+	return {
+		deleteLesson: deleteLesson,
+		editLesson: editLesson
+	};
+
+} ();
