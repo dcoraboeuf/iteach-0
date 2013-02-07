adc4b49779e6f6ffd2e5ff848da9444d3403ff5f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-07 19:48:26 +0100

    #32 More unit tests for the validation

--- iteach-core/src/test/java/net/iteach/core/model/PreferenceKeyTest.java
+++ iteach-core/src/test/java/net/iteach/core/model/PreferenceKeyTest.java
@@ -37,5 +37,69 @@ public class PreferenceKeyTest {
 		assertNotNull(message);
 		assertEquals("L'heure minimale pour le planning doit être entre 1 et 11.", message.getLocalizedMessage(strings, Locale.FRENCH));
 	}
+	
+	@Test
+	public void min_time_invalid_12() {
+		Localizable message = PreferenceKey.PLANNING_MIN_TIME.validate("12");
+		assertNotNull(message);
+		assertEquals("L'heure minimale pour le planning doit être entre 1 et 11.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void min_time_invalid_mm() {
+		Localizable message = PreferenceKey.PLANNING_MIN_TIME.validate("mm");
+		assertNotNull(message);
+		assertEquals("L'heure minimale pour le planning doit être un entier.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void min_time_invalid_double() {
+		Localizable message = PreferenceKey.PLANNING_MIN_TIME.validate("10.5");
+		assertNotNull(message);
+		assertEquals("L'heure minimale pour le planning doit être un entier.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void max_time_valid_1() {
+		assertNull(PreferenceKey.PLANNING_MAX_TIME.validate("13"));
+	}
+	
+	@Test
+	public void max_time_valid_6() {
+		assertNull(PreferenceKey.PLANNING_MAX_TIME.validate("20"));
+	}
+	
+	@Test
+	public void max_time_valid_11() {
+		assertNull(PreferenceKey.PLANNING_MAX_TIME.validate("23"));
+	}
+	
+	@Test
+	public void max_time_invalid_12() {
+		Localizable message = PreferenceKey.PLANNING_MAX_TIME.validate("12");
+		assertNotNull(message);
+		assertEquals("L'heure maximale pour le planning doit être entre 13 et 23.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void max_time_invalid_24() {
+		Localizable message = PreferenceKey.PLANNING_MAX_TIME.validate("24");
+		assertNotNull(message);
+		assertEquals("L'heure maximale pour le planning doit être entre 13 et 23.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void max_time_invalid_mm() {
+		Localizable message = PreferenceKey.PLANNING_MAX_TIME.validate("mm");
+		assertNotNull(message);
+		assertEquals("L'heure maximale pour le planning doit être un entier.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
+	
+	@Test
+	public void max_time_invalid_double() {
+		Localizable message = PreferenceKey.PLANNING_MAX_TIME.validate("14.5");
+		assertNotNull(message);
+		assertEquals("L'heure maximale pour le planning doit être un entier.", message.getLocalizedMessage(strings, Locale.FRENCH));
+	}
 
 }
