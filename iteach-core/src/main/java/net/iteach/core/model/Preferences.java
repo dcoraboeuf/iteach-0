06117e34d3062e52fc6e743a7c432298ab00e721
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-17 20:00:45 +0100

    #33 Getting the value for the preferences in the profile page

--- iteach-core/src/main/java/net/iteach/core/model/Preferences.java
+++ iteach-core/src/main/java/net/iteach/core/model/Preferences.java
@@ -2,11 +2,21 @@ package net.iteach.core.model;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class Preferences {
 
-    private final Map<PreferenceKey, String> map;
+    private final Map<String, String> map;
+
+    public Preferences(Map<PreferenceKey, String> map) {
+        Map<String, String> m = new HashMap<>();
+        for (PreferenceKey key : map.keySet()) {
+            String value = map.get(key);
+            m.put(key.name(), value);
+        }
+        this.map = m;
+    }
 
 }
