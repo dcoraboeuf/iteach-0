55e9521ee0ee38c02c03b37d383feebb397792ab
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-02 20:59:17 +0100

    #84 Correct encoding for the response

--- iteach-web/src/main/java/net/iteach/web/admin/AdminController.java
+++ iteach-web/src/main/java/net/iteach/web/admin/AdminController.java
@@ -1,8 +1,5 @@
 package net.iteach.web.admin;
 
-import java.io.IOException;
-import java.util.Locale;
-
 import net.iteach.api.admin.AccountSummary;
 import net.iteach.api.admin.AdminService;
 import net.iteach.api.admin.SettingsUpdate;
@@ -13,7 +10,6 @@ import net.iteach.utils.InputException;
 import net.iteach.web.support.AbstractGUIController;
 import net.iteach.web.support.ErrorHandler;
 import net.sf.jstring.Strings;
-
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,95 +20,98 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController extends AbstractGUIController {
 
-	private final AdminService adminService;
-	private final Strings strings;
+    private final AdminService adminService;
+    private final Strings strings;
     private final ObjectMapper mapper;
-		
-	@Autowired
-	public AdminController(ErrorHandler errorHandler, AdminService adminService, Strings strings, ObjectMapper mapper) {
-		super(errorHandler);
-		this.adminService = adminService;
-		this.strings = strings;
+
+    @Autowired
+    public AdminController(ErrorHandler errorHandler, AdminService adminService, Strings strings, ObjectMapper mapper) {
+        super(errorHandler);
+        this.adminService = adminService;
+        this.strings = strings;
         this.mapper = mapper;
     }
 
-	/**
-	 * Settings
-	 */
-	@RequestMapping(value = "/settings", method = RequestMethod.GET)
-	public String settings(Model model) {
-		// Loads the settings
-		model.addAttribute("settings", adminService.getSettings());
-		// OK
-		return "admin/settings";
-	}
-	
-	/**
-	 * Uploads the settings
-	 */
-	@RequestMapping(value = "/settings", method = RequestMethod.POST)
-	public String setSettings(Model model, WebRequest request, Locale locale) {
-		// Gets all keys
-		SettingsUpdate update = new SettingsUpdate();
-		for (ConfigurationKey key: ConfigurationKey.values()) {
-			String[] values = request.getParameterValues(key.name());
-			if (values != null && values.length == 1) {
-				update = update.withValue(key, values[0]);
-			}
-		}
-		// Loads the settings
-		model.addAttribute("settings", adminService.getSettings());
-		try {
-			// Sends the update
-			adminService.setSettings(update);
-			// Message
-			model.addAttribute("message", UserMessage.success(strings.get(locale, "admin.settings.ok")));
-		} catch (InputException ex) {
-			// Message
-			model.addAttribute("message", UserMessage.error(errorHandler.displayableError(ex, locale)));
-		}
-		// OK
-		return "admin/settings";
-	}
+    /**
+     * Settings
+     */
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    public String settings(Model model) {
+        // Loads the settings
+        model.addAttribute("settings", adminService.getSettings());
+        // OK
+        return "admin/settings";
+    }
 
-	/**
-	 * List of accounts
-	 */
-	@RequestMapping(value = "/accounts", method = RequestMethod.GET)
-	public String accounts(Model model) {
-		// List of account summaries
-		model.addAttribute("accounts", adminService.getAccounts());
-		// OK
-		return "admin/accounts";
-	}
-	
-	/**
-	 * Page to confirm the deletion
-	 */
-	@RequestMapping(value = "/account/{id}/delete", method = RequestMethod.GET)
-	public String accountDeleteForm(Model model, @PathVariable int id) {
-		// Loads user details
-		AccountSummary details = adminService.getAccount(id);
-		model.addAttribute("details", details);
-		// OK
-		return "admin/accountDelete";
-	}
-	
-	/**
-	 * Deletes the user
-	 */
-	@RequestMapping(value = "/account/{id}/delete", method = RequestMethod.POST)
-	public String accountDelete(Model model, @PathVariable int id) {
-		// Deletes the user
-		adminService.deleteAccount(id);
-		// OK
-		return "redirect:/admin/accounts";
-	}
+    /**
+     * Uploads the settings
+     */
+    @RequestMapping(value = "/settings", method = RequestMethod.POST)
+    public String setSettings(Model model, WebRequest request, Locale locale) {
+        // Gets all keys
+        SettingsUpdate update = new SettingsUpdate();
+        for (ConfigurationKey key : ConfigurationKey.values()) {
+            String[] values = request.getParameterValues(key.name());
+            if (values != null && values.length == 1) {
+                update = update.withValue(key, values[0]);
+            }
+        }
+        // Loads the settings
+        model.addAttribute("settings", adminService.getSettings());
+        try {
+            // Sends the update
+            adminService.setSettings(update);
+            // Message
+            model.addAttribute("message", UserMessage.success(strings.get(locale, "admin.settings.ok")));
+        } catch (InputException ex) {
+            // Message
+            model.addAttribute("message", UserMessage.error(errorHandler.displayableError(ex, locale)));
+        }
+        // OK
+        return "admin/settings";
+    }
+
+    /**
+     * List of accounts
+     */
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public String accounts(Model model) {
+        // List of account summaries
+        model.addAttribute("accounts", adminService.getAccounts());
+        // OK
+        return "admin/accounts";
+    }
+
+    /**
+     * Page to confirm the deletion
+     */
+    @RequestMapping(value = "/account/{id}/delete", method = RequestMethod.GET)
+    public String accountDeleteForm(Model model, @PathVariable int id) {
+        // Loads user details
+        AccountSummary details = adminService.getAccount(id);
+        model.addAttribute("details", details);
+        // OK
+        return "admin/accountDelete";
+    }
+
+    /**
+     * Deletes the user
+     */
+    @RequestMapping(value = "/account/{id}/delete", method = RequestMethod.POST)
+    public String accountDelete(Model model, @PathVariable int id) {
+        // Deletes the user
+        adminService.deleteAccount(id);
+        // OK
+        return "redirect:/admin/accounts";
+    }
 
     /**
      * Exports the data for the user
@@ -121,11 +120,16 @@ public class AdminController extends AbstractGUIController {
     public void accountExport(@PathVariable int id, HttpServletResponse response) throws IOException {
         // Gets a copy of the account
         ExportedTeacher copy = adminService.export(id);
-        // Serializes as JSON
-        mapper.writeValue(response.getOutputStream(), copy);
         // Headers
-        response.addHeader("Content-Type", "application/json");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         response.addHeader("Content-Disposition", "attachment; filename=account.json");
+        // Serializes as JSON
+        mapper.writeValue(
+                new OutputStreamWriter(
+                        response.getOutputStream(),
+                        "UTF-8"),
+                copy);
     }
 
 }
