d67bcc43e3597d78b731d76e5e10a6161d5766e2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-02 21:25:55 +0100

    #84 HRate exported as amount + currency code

--- iteach-service-api/src/main/java/net/iteach/api/model/copy/ExportedSchool.java
+++ iteach-service-api/src/main/java/net/iteach/api/model/copy/ExportedSchool.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import net.iteach.core.model.Coordinate;
 import org.joda.money.Money;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -13,14 +14,16 @@ public class ExportedSchool extends ExportedWithCoordinates {
 
     private final String name;
     private final String color;
-    private final Money hrate;
+    private final BigDecimal hrate;
+    private final String currency;
     private final List<ExportedStudent> students;
 
     public ExportedSchool(List<ExportedComment> comments, List<Coordinate> coordinates, String name, String color, Money hrate, List<ExportedStudent> students) {
         super(comments, coordinates);
         this.name = name;
         this.color = color;
-        this.hrate = hrate;
+        this.hrate = hrate.getAmount();
+        this.currency = hrate.getCurrencyUnit().getCurrencyCode();
         this.students = students;
     }
 }
